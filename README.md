87e9fa15ee51e5c7fc2432d55b0e13714102cbea
rodriguezemautn <erodriguezrodriguez@alu.frlp.utn.edu.ar>
2022-10-24 06:36:10 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Trapitos
+## Metodologias agiles 2022
+## Integrantes
+### Juan Cruz
+### Santiago
+### Jeronimo
+### Emanuel
+
+# Notas
+### Comandos
+
+npm start
+
+npm run dev  (nodemon)
+
+### Depencias
+
+npm i express express-handlebars morgan firebase-admin dotenv
+
+npm i nodemon - D
+
+ver path absoluto en .env
+
---
+++ src/routes/index.js
@@ -0,0 +1,140 @@
+const { Router } = require('express');
+const {db} = require('../firebase');
+
+const router = Router();
+
+//recupera todas las direcciones
+router.get("/", async (req, res) => {
+    const querySnapshot = await db.collection("direcciones").get();
+
+    const direcciones = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+
+    console.log(direcciones);
+
+    
+    //res.json(direcciones); //retorna json
+    res.render('index', {direcciones}) //renderiza con handelbar
+});
+
+//recupera una direccion por id
+router.get("/:id", async (req, res) => {
+    
+
+    const direccion = await db.collection('direcciones').doc(req.params.id).get()
+
+    //randeriza con handelbar
+    //res.render("index", { direccion: { id: direccion.id, ...direccion.data() }});
+    
+    res.json(direccion.data());
+
+   
+});
+
+//recupera una direccion por poximidad
+router.get("/:calle/:entre1/:entre2", async (req, res) => {
+    
+    const { calle } = req.params;   
+    var callep = parseInt(calle);
+    var callei = callep--;
+    var callef = callep++; 
+    const { entre1 } = req.params;
+    var entre1p = parseInt(entre1);
+    var entre1i = entre1p--;
+    var entre1f = entre1p++; 
+    const { entre2 } = req.params;
+    
+    const querySnapshot = await db.collection('direcciones')
+    .where('Calle', 'in', [callei, callep, callef])
+    .where('entre1', '==', entre1p).get(); 
+
+    
+    const direcciones = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+
+    console.log(direcciones);
+
+    
+    //res.json(direcciones); //retorna json
+    res.render('index', {direcciones}) //renderiza con handelbar
+    
+});
+
+//carga una direccion nueva
+router.post('/new-direccion', async (req, res ) => {
+    //const { Calle, entre1, entre2, estado } = req.body
+    
+     //------------carga direccion-----------
+    console.log(req.body);
+    const direccion = await db.collection("direcciones").add({
+        Calle: parseInt(req.body.Calle),
+        entre1: parseInt(req.body.entre1),
+        entre2: parseInt(req.body.entre2),
+        estado: req.body.estado
+    });
+    //------------retorna message-----------------------------------
+    if(res.statusCode == 200){
+        res.json({status: res.statusCode, message: "Direccion cargada"})
+    }
+    if(res.statusCode != 200){
+        res.json({status: res.statusCode, message: "Error al cargar direccion"})
+    } 
+    //---------------------------------------------------------  
+    
+    //res.redirect('/') //redirecciona a localhost:3000 
+})
+
+//devuelve una direccion para su edicion 
+router.get('/edit-direccion/:id', async (req, res) => {
+    const doc = await db.collection('direcciones').doc(req.params.id).get()
+
+    // renderiza la direccion de id igual al parametro en vista handelbar
+    res.render("index", { direccion: { id: doc.id, ...doc.data() }});
+
+    //res.json(doc.data()) //retorna un json con una tupla direccion 
+})
+
+//borra un direccion eliminandola de la bd
+router.get('/delete-direccion/:id', async (req, res) => {
+    
+    const doc = await db.collection('direcciones').doc(req.params.id).delete()    
+    //---------------retorna menssage luego de cargar------------
+   // if(res.statusCode == 200){
+   //     res.json({status: res.statusCode, message: "Direccion borrada"})
+   // }
+   // if(res.statusCode != 200){
+   //     res.json({status: res.statusCode, message: "Error al borrar"})
+   // } 
+    //---------------------------------------
+
+    res.redirect('/') //redirecciona a localhost:3000
+})
+
+
+//actualiza todos los campos de una direccion segun su id 
+router.post("/update-direccion/:id", async (req, res) => {
+    
+    const { id } = req.params;
+    
+    const direcccion = await db.collection("direcciones").doc(id).update(req.body);
+    //------------actualiza direccion retorna message-----------------------------------
+   // if(res.statusCode == 200){
+   //     res.json({status: res.statusCode, message: "Direccion cargada"})
+   // }
+   // if(res.statusCode != 200){
+   //     res.json({status: res.statusCode, message: "Error al cargar direccion"})
+   // } 
+    //---------------------------------------------------------    
+
+    res.redirect("/"); //redirecciona a localhost:3000
+
+})
+
+
+
+
+module.exports = router;
